bf33b45d86f0006b175892d0b4dc241e887cfab9
Matide Bravo <matildeopbravo@gmail.com>
2021-05-17 15:49:36 +0100

    Added backbone of aurrasd

--- src/aurrasd.c
+++ src/aurrasd.c
@@ -1,7 +1,17 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+void sigter_handler(int signum) {
+    printf("Finishing up current requests. No more requests allowed.\n");
+    // codigo de término
+}
+
 int main(int argc, char* argv[]) {
-    printf("Hello world\n");
+    if (signal(SIGTERM, sigter_handler) == SIG_ERR) {
+        perror("SIGQUIT");
+        return 1;
+    }
+    // start_server(); {}
 }
 
